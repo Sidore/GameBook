34b11510cde2bfc2cfc996b2dd6a11c07d5fda57
Vladislav Sydorenko <v.sydorenko@bbq.agency>
2019-06-12 11:11:51 +0300

    fix

--- src/server/app.ts
+++ src/server/app.ts
@@ -60,6 +60,7 @@ export class GameBookApp {
 
         server.use(function(req, res, next) {
             res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE")
             res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
             next();
           });
