6b0d7cfd5ba1edfdb3df0aeb30759cea210e9fb0
Vladislav Sydorenko <v.sydorenko@bbq.agency>
2019-06-15 00:27:18 +0300

    created others layouts

---
+++ src/client/Layouts/Hall.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+
+export default class HallLayout extends React.Component {
+
+    state = {
+        newUser : false
+    }
+
+    toggleHandler(params) {
+        this.setState({
+            newUser: !this.state.newUser
+        })
+    }
+
+    render() {
+
+        let form;
+
+        if (this.state.newUser) {
+            form = <div>
+                        login form
+                    </div>
+        } else {
+            form = <div>
+                        reg form
+                    </div>
+        }
+
+        return(
+            <div>
+                <div>form type toggle <br/>
+                    <input type="checkbox" onChange={this.toggleHandler}/>
+                </div>
+                
+                {form}
+            </div>
+            
+        );
+    }
+
+}
