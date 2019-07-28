7421f36cb412d55b0d48d61b413f123b372bc1be
Vladislav Sydorenko <v.sydorenko@bbq.agency>
2019-07-28 16:26:17 +0300

    email vefify

---
+++ src/models/Token/index.ts
@@ -0,0 +1,17 @@
+import { Schema, Model, model, Document } from "mongoose";
+
+
+export interface IToken {
+    token: string;
+    _userId: string;
+}
+
+export interface TokenType extends Document, IToken {};
+
+const tokenSchema = new Schema({
+    _userId: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
+    token: { type: String, required: true },
+    createdAt: { type: Date, required: true, default: Date.now, expires: 43200 }
+});
+
+export let Token = model<TokenType>("token", tokenSchema);
