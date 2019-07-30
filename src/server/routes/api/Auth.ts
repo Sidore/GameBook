81d805de24ec746ddb2dfefc7451507a340a3f33
Vladislav Sydorenko <v.sydorenko@bbq.agency>
2019-07-30 13:04:08 +0300

    fix token logic

--- src/server/routes/api/Auth.ts
+++ src/server/routes/api/Auth.ts
@@ -62,15 +62,19 @@ export const ConfirmRoutes = Router().get("/:token", (req, res) => {
         if (!token) return res.status(400).send({ type: 'not-verified', data: 'We were unable to find a valid token. Your token my have expired.' });
  
         // If we found a token, find a matching user
-        User.findOne({ _id: token._userId, email: req.body.email }, function (err, user) {
-            if (!user) return res.status(400).send({ msg: 'We were unable to find a user for this token.' });
+        User.findOne({ _id: token._userId }, function (err, user) {
+            if (!user) return res.status(400).send({ msg: 'We were unable to find a user for this token.', data : {
+                // user : token._userId,
+                token: req.params.token,
+                tokenObj : token
+            } });
             if (user.isVerified) return res.status(400).send({ type: 'already-verified', msg: 'This user has already been verified.' });
  
             // Verify and save the user
             user.isVerified = true;
             user.save(function (err) {
                 if (err) { return res.status(500).send({ msg: err.message }); }
-                res.status(200).send("The account has been verified. Please log in.");
+                res.redirect('../login');
             });
         });
     });
