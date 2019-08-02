18f6e97255f1cef77e57cffd56fad2c59877398f
Vladislav Sydorenko <v.sydorenko@bbq.agency>
2019-08-02 18:24:34 +0300

    setup apollo and react-apollo

--- src/client/Layouts/index.tsx
+++ src/client/Layouts/index.tsx
@@ -4,6 +4,9 @@ import Lobby from "./LobbyPage";
 import Auth from "./AuthPage";
 import Game from "./GamePage";
 
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 
@@ -13,6 +16,9 @@ import "./index.styl";
 
 import { BrowserRouter as Router, Route, Link, Redirect , Switch} from "react-router-dom";
 
+const client = new ApolloClient({
+  uri: "/graphql"
+})
 
 const store = createStore(rootReducer);
 
@@ -46,42 +52,38 @@ export default class MainLayout extends React.Component {
   render () {
     return (
         <Provider store={store}>
-          <Router>
-            <div className="root-div">
-              <Switch>
-              
-              <Route path="/login" render={() => (
-                  this.state.token ? 
-                    (<Redirect to="/"/>) :
-                    (<Auth onToken={this.tokenHandler} onUser={this.userHandler}/>)
-                  )} 
-                />            
-
-              <Route path="/" exact render={() => (
-                  !this.state.token ? 
-                    (<Redirect to="/login"/>) : 
-                    (<Lobby token={this.state.token} user={this.state.user}/>)
-                  )}
-                />
-
-              <Route path="/rooms/:id" render={(props) => (
-                  !this.state.token ?
-                  (<Redirect to="/login"/>) : 
-                  (<Game token={this.state.token} {...props}/>)
-                )} 
-                />
-
-              <Route render={() => (
-                  !this.state.token ? 
-                    (<Redirect to="/login"/>) : 
-                    (<Lobby token={this.state.token} user={this.state.user}/>)
-                  )}
-                />
-
-
+          <ApolloProvider client={client}>
+            <Router>
+              <div className="root-div">
+                <Switch>
+                  <Route path="/login" render={() => (
+                      this.state.token ? 
+                        (<Redirect to="/"/>) :
+                        (<Auth onToken={this.tokenHandler} onUser={this.userHandler}/>)
+                      )} 
+                    />            
+                  <Route path="/" exact render={() => (
+                      !this.state.token ? 
+                        (<Redirect to="/login"/>) : 
+                        (<Lobby token={this.state.token} user={this.state.user}/>)
+                      )}
+                    />
+                  <Route path="/rooms/:id" render={(props) => (
+                      !this.state.token ?
+                      (<Redirect to="/login"/>) : 
+                      (<Game token={this.state.token} {...props}/>)
+                    )} 
+                    />
+                  <Route render={() => (
+                      !this.state.token ? 
+                        (<Redirect to="/login"/>) : 
+                        (<Lobby token={this.state.token} user={this.state.user}/>)
+                      )}
+                    />  
                 </Switch>
-            </div>
-          </Router>
+              </div>
+            </Router>
+          </ApolloProvider>
         </Provider>
     );
   }
