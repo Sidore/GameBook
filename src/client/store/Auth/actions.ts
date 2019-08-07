54f795d5bc6ab8f4f04a68ef292be11bd5053637
Vladislav Sydorenko <v.sydorenko@bbq.agency>
2019-08-07 21:43:01 +0300

    token actions

--- src/client/store/Auth/actions.ts
+++ src/client/store/Auth/actions.ts
@@ -1,5 +1,17 @@
-import { TokenState, TokenActionTypes, SET_TOKEN, REMOVE_TOKEN} from "./types";
+import { TokenState, TokenActionTypes, SET_TOKEN, REMOVE_TOKEN } from "./types";
 
 import { ThunkAction, ThunkDispatch } from "redux-thunk";
 import { AnyAction } from "redux";
 
+function setToken(token: string): TokenActionTypes {
+    return {
+        type: SET_TOKEN,
+        payload : token
+    }
+}
+
+function removeToken(): TokenActionTypes {
+    return {
+        type: REMOVE_TOKEN
+    }
+}
